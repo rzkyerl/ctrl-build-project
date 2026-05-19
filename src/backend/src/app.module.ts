9cc5ba7c7a33fe50ac52b7d6176635303a38d7cf
sultonisky <muhsultonipml111@gmail.com>
2026-05-19 21:48:37 +0700

    feat(backend): setup backend resources prisma and database

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PortfolioModule } from './portfolio/portfolio.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PortfolioModule],
+  imports: [PortfolioModule, PrismaModule, UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
